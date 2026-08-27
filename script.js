378507f4e62a47b210259e31e4f97dcd2bc74b6a
natnicha162551 <sunchai.zab@gmail.com>
2026-08-27 14:35:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 // เพิ่ม Javascript สำหรับการโต้ตอบกับผู้ใช้ (optional)
 document.addEventlistener('DOMContentLoaded',function () {
-   console.log('portfolio ของ SOMCHAI พร้อมแล้ว!');
+   console.log('portfolio ของ โญฑิตา พร้อมแล้ว!');
 });
